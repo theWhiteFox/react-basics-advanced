be7c358f6b2e6a328a34d9a1e6f83aff8ec053bf
Ó Conchubhair ♔ <stevo.joc@gmail.com>
2020-06-16 06:16:49 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react component
+const App = () => {
+  return <div>Hello There!</div>;
+};
+
+// Take the react component and display it
+ReactDOM.render(<App />, document.querySelector('#root'));
