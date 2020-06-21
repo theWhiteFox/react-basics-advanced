995a9c2d0c0b7fd63d75e248c65655cc03a5f552
Ó Conchubhair ♔ <stevo.joc@gmail.com>
2020-06-21 09:33:02 +0100

    Message Segment components children props

--- src/index.js
+++ src/index.js
@@ -4,11 +4,35 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
+import Message from './Message';
+import Segment from './Segment';
+import SegmentDetail from './SegmentDetail';
 
 // Create a react component
 const App = () => {
   return (
     <div className="ui container comments">
+      <Message
+        header="Changes in Service"
+        content="We just updated our privacy policy here to better service our customers"
+      />
+      <div>
+        <Segment>
+          <div className="ui icon header">
+            <i className="pdf file outline icon"></i>
+            No documents are listed for this customer
+          </div>
+          <div className="ui primary button">Add Document</div>
+        </Segment>
+        <Segment>
+          <SegmentDetail
+            header="For Your Information"
+            text="I'm baby photo booth offal tofu gastropub. 
+            Humblebrag cronut palo santo four loko DIY. Organic sartorial leggings artisan, 
+            slow-carb chillwave vaporware hell of ugh snackwave shoreditch tumblr"
+          />
+        </Segment>
+      </div>
       <ApprovalCard>
         <CommentDetail
           author="Sam"
