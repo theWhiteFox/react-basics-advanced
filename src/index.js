a0d7a932c1a14c0f661177683289b3a68eb684f2
Ó Conchubhair ♔ <stevo.joc@gmail.com>
2020-06-23 09:06:32 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,22 +8,37 @@ class App extends React.Component {
     super(props);
 
     // only direct assignment to state
-    this.state = { lat: null };
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         // to update object called setstate
-        this.setState({ lat: position.coords.latitude });
+        this.setState({
+          lat: position.coords.latitude
+        });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({
+          errorMessage: err.message
+        });
+      }
     );
   }
 
   // React says we have to define render !!
   render() {
-    return <div>Latitude: {this.state.lat} </div>;
+    return ( 
+      <div>
+      Latitude:{this.state.lat} 
+      <br />
+      Error: {this.state.errorMessage } 
+      </div>
+    );
   }
 }
 
 // Take the react component and display it
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render( < App / > , document.querySelector('#root'));
