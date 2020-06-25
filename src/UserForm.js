83d48b31c2eaa8ff61d4d2d37f1fcbddde92311e
Ó Conchubhair ♔ <stevo.joc@gmail.com>
2020-06-25 09:11:32 +0100

    Add files via upload

---
+++ src/UserForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class UserForm extends React.Component {
+    render() {
+        return (
+            <div >
+                <form className="user-form">
+                    <h1><input placeholder="Choose one ?" /></h1>
+                </form >
+            </div>
+        )
+    }
+}
+
+export default UserForm;
