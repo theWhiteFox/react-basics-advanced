438038777206c78fa106f14e2d7bc4e49bb8e1d7
Ó Conchubhair ♔ <stevo.joc@gmail.com>
2020-06-29 23:05:27 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,17 @@ import SearchBar from './SearchBar'
 
 
 class App extends React.Component {
+
+    onSearchSubmit = (term) => {
+        console.log(term)
+    }
+
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
-                <SearchBar />
+                <SearchBar onSubmit={this.onSearchSubmit} />
             </div>
-        )
+        );
     }
 }
 
