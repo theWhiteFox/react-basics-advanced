a7cdb3abc4317058a9b15eb18f5fb6c6e26b397c
Ó Conchubhair ♔ <stevo.joc@gmail.com>
2020-07-03 08:38:36 +0100

    axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
-
 
 class App extends React.Component {
 
     onSearchSubmit = (term) => {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 'Client-ID L9ZE3QdwabSoIYvuIJrcTdnp98jfH8kZKLBBtQT3F5s'
+            }
+        });
         console.log(term)
     }
 
